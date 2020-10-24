b0d4e7774c0a0354b839431dbeee48c4391fcce4
BeixiJia <240793032@qq.com>
2020-10-24 21:53:33 +0800

    初步移动和旋转

---
+++ WeThePenguins/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    public int camp;
+    private Unit_Base unit;
+    private void Awake()
+    {
+        unit = GetComponent<Unit_Base>();
+    }
+    void Update()
+    {
+        float x = 0;
+        if (camp == 0)
+        {
+            x = Input.GetAxis("Horizontal");
+        }
+       else if (camp == 1)
+        {
+            x = Input.GetAxis("Horizontal2");
+        }
+        unit.InputMovement(x);
+    }
+}
---
+++ WeThePenguins/Assets/Scripts/Unit_Base.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit_Base : MonoBehaviour
+{
+    [SerializeField]
+    private float angularSpeed; //角速度
+    [SerializeField]
+    private float k; //弹性系数
+    private float degree; //当前角度
+
+    [SerializeField]
+    private float maxSpeed;
+    [SerializeField]
+    private float angularSpeedChange;
+    private float speed;
+    [SerializeField]
+    private float accelerate;
+
+
+    [SerializeField]
+    private Transform bodyTransform;
+    private void FixedUpdate()
+    {
+        float currentDegree = degree;
+        degree += angularSpeed * Time.fixedDeltaTime;
+        angularSpeed += k * currentDegree * Time.fixedDeltaTime;
+        bodyTransform.eulerAngles = new Vector3(0, 0, degree);
+
+        transform.position += speed * Time.fixedDeltaTime * Vector3.right;
+    }
+
+    public void InputMovement(float x)
+    {
+        if (x != 0)
+        {
+            speed += x * accelerate * Time.deltaTime;
+            if (speed > maxSpeed)
+            {
+                speed = maxSpeed;
+            }
+            else if (speed < -maxSpeed)
+            {
+                speed = -maxSpeed;
+            }
+            else
+            {
+                angularSpeed -= x * angularSpeedChange * Time.deltaTime;
+            }
+        }
+    }
+}
